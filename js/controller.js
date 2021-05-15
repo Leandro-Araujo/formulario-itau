77766efee67633edccf6545a6a05c7fe63fdb005
Leandro Silva Araujo <leandro_sy@hotmail.com>
2021-05-15 15:14:34 -0300

    Colocando verificador de cpf e caixinhas de sinalizacao de erro e sucesso

--- js/controller.js
+++ js/controller.js
@@ -14,8 +14,25 @@ function verificarObrigatorios(campos){
 	return verificado;
 }
 
-function autenticaCPF(strCPF){
-
+function autenticaCPF(cpf){
+	let padraocpf = /[0-9]{11}/;
+	let compara = padraocpf.exec(cpf.value);
+	if(cpf.length != 11)	return false;
+	else{
+		var numeros = cpf.substring(0, 9);
+		var digitos = cpf.substring(9);
+		var soma = 0;
+		for(var i = 10; i > 1; i--)		soma += numeros.charAt(10 - i) * i;		
+		// validacao do primeiro digito
+		var resultado = soma % 11 < 2 ? 0 : 11- (soma % 11);
+		if(resultado != digitos.charAt(0))	return false;		
+		soma = 0;
+		numeros = cpf.substring(0,10);
+		for(var k = 11; k > 1; k--)		soma += numeros.charAt(11 - k) * k;		
+		resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+		if(resultado != digitos.charAt(1))	return false;
+		return true;
+	}
 }
 
 function verificaCPF(cpf){
@@ -60,7 +77,12 @@ function enviarFormulario(evt, campos) {
 
 
     if(verifica){
-    	console.log(autenticaCPF(cpf));
+    	if(autenticaCPF(cpf)){
+			document.getElementById('alertaerror').style.display = "none";	
+		}
+		else{
+			document.getElementById('alertaerror').style.display = "block";
+		}
     }
 	
 	evt.preventDefault();
